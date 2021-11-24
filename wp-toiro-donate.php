e4684b2ce998dd39b59d9d78e17db927be75c8e2
Takeshi FURUTA <furuta@mq-sol.jp>
2021-11-24 04:25:45 +0000

    donate add

--- wp-toiro-donate.php
+++ wp-toiro-donate.php
@@ -78,8 +78,8 @@ function donate_add_pages() {
 function donate_menu_index() {
 	global $wpdb;
 	$dir = plugin_dir_path( __FILE__ );
-	$form_file = $dir . "/form.html"; 
-	$tbody_file = $dir . "/tbody.html";
+	$form_file = $dir . "form.html"; 
+	$tbody_file = $dir . "tbody.html";
 	$tbody_tpl = file_get_contents($tbody_file);
 	$main = file_get_contents($form_file);
 	$tbody = "";
@@ -201,6 +201,80 @@ function donate_sumbit() {
  */
 function donate_submenu_page1() {
 	echo '<h2>寄付確認</h2>';
+	global $wpdb;
+	$dir = plugin_dir_path( __FILE__ );
+	$form_file = $dir . "donate_table.html";
+	$tbody_file = $dir . "donate_list.html";
+
+	$tbody_tpl = file_get_contents($tbody_file);
+	$main = file_get_contents($form_file);
+	echo "<pre";
+	echo htmlspecialchars($tbody_tpl);
+	echo "</pre>";
+	$tbody = "";
+	try {
+		$query = "SELECT d.*, p.project_name, p.project_code, t.payment_type as payment_name
+		FROM wp_donate AS d
+		LEFT JOIN wp_donate_project as p on (p.id = d.donate_project_id)
+		LEFT JOIN wp_payment_type as t on (t.id = d.payment_type_id)
+		WHERE d.del_flag = 0";
+		$results = $wpdb->get_results( $query );
+
+	} catch (Exception $e) {
+		echo '捕捉した例外: ',  $e->getMessage(), "\n";
+	}
+	if  (!is_null( $results ))  {
+		$i=0;
+		foreach($results as $row) {
+
+			$id = $row->id;
+			$donate_project_id = $row->donate_project_id;
+			$payment_id = $row->payment_id;
+			$donor_email = $row->donor_email;		
+			$donor_name = $row->donor_name;
+			$donor_zip = $row->donor_zip;
+			$donor_address = $row->donor_address;
+			$donor_tel = $row->donor_tel;
+			$token = $row->token;
+			$price = $row->price;
+			$tax = $row->tax;
+			$charge = $row->charge;
+			$payment_type_id = $row->payment_type_id;
+			$payment_date = $row->payment_date;
+			$del_flag = $row->del_flag;
+			$creator = $row->creator;
+			$create_date = $row->create_date;
+			$moderator = $row->moderator;
+			$update_date = $row->update_date;
+			$project_name = $row->project_name;
+			$project_code = $row->project_code;
+			
+			$row_data = $tbody_tpl;
+			$row_data = str_replace('%I%', ++$i, $row_data);
+			$row_data = str_replace('%ID%', $id, $row_data);
+			$row_data = str_replace('%PROJECT_CODE%', $project_code, $row_data);
+			$row_data = str_replace('%PROJECT_NAME%', $project_name, $row_data);
+			$row_data = str_replace('%PAYMENT_ID%', $payment_id, $row_data);
+			$row_data = str_replace('%DONOR_NAME%', $donor_name, $row_data);
+			$row_data = str_replace('%DONOR_ZIP%', $donor_zip, $row_data);
+			$row_data = str_replace('%DONOR_ADDRESS%', $donor_address, $row_data);
+			$row_data = str_replace('%DONOR_EMAIL%', $donor_email, $row_data);
+			$row_data = str_replace('%DONOR_TEL%', $donor_tel, $row_data);
+			$row_data = str_replace('%TOKEN%', $token, $row_data);
+			$row_data = str_replace('%PRICE%', $price, $row_data);
+			$row_data = str_replace('%CHARGE%', $charge, $row_data);
+			$row_data = str_replace('%TAX%', $tax, $row_data);
+			$row_data = str_replace('%PAYMENT_TYPE%', $payment_type, $row_data);
+			$tbody .= $row_data;
+			
+		}
+	}
+	$main = str_replace("%TBODY%", $tbody, $main);
+	echo $main;	
+
+
+
+
 }
 
 /**
